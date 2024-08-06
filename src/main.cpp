6ca8953b0a9ce071e0469d0325d128b2cdb0e7b6
Steve Faletti <stevefaletti@gmail.com>
2024-08-06 16:36:21 -0400

    Use arrays for the encoder

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,9 @@
 #include <Encoder.h>
 #include <Bounce2.h>
 
-Encoder encoder1(11, 12);
-Encoder encoder2(9, 10);
-int encVal1 = 0;
-int encVal2 = 0;
-
 // Set up more buttons than needed at moment, just in case things on the hardware side change. Physical buttons are connected to pins 0-6 on the Teensy 3.2
 #define BUTTON_COUNT 6
+#define FIRST_BUTTON_PIN 0
 Bounce *buttons[BUTTON_COUNT];
 
 const int KEY_MAP[BUTTON_COUNT] = {
@@ -20,44 +16,69 @@ const int KEY_MAP[BUTTON_COUNT] = {
     KEY_P, // p2 start
 };
 
+// Set up encoders. Physical encoders are connected to pins 9-12 on the Teensy 3.2
+#define ENCODER_COUNT 2
+#define FIRST_ENCODER_PIN 9
+Encoder *encoders[ENCODER_COUNT];
+int32_t encoderValues[ENCODER_COUNT];
+
+const int ENCODER_MAP[ENCODER_COUNT * 2] = {
+    KEY_A,           // p1 d1
+    KEY_D,           // p1 d2
+    KEY_LEFT_ARROW,  // p2 d1
+    KEY_RIGHT_ARROW, // p2 d2
+};
+
 void setup()
+// Initialize buttons
 {
   for (int i = 0; i < BUTTON_COUNT; i++)
   {
-    buttons[i] = new Bounce();
-    buttons[i]->attach(i, INPUT_PULLUP);
-    buttons[i]->interval(5);
+    int index = FIRST_BUTTON_PIN + i;
+    buttons[index] = new Bounce();
+    buttons[index]->attach(index, INPUT_PULLUP);
+    buttons[index]->interval(5);
   }
+
+  // Intialize encoders.
+  for (int i = 0; i < ENCODER_COUNT; i++)
+  {
+    int index = FIRST_ENCODER_PIN + (i * ENCODER_COUNT);
+    encoders[i] = new Encoder(index, index + 1);
+    encoderValues[i] = 0;
+  }
+
+  Keyboard.begin();
 }
 
 void loop()
 {
-  int encVal1 = encoder1.readAndReset();
-  int encVal2 = encoder2.readAndReset();
-  // Mouse.move(encVal1 * 10, encVal2 * 10);
-
-  if (encVal1 > 0)
+  // Read encoder values
+  for (int i = 0; i < ENCODER_COUNT; i++)
   {
-    Keyboard.press(KEY_A);
-    Keyboard.release(KEY_A);
-  }
-  else if (encVal1 < 0)
-  {
-    Keyboard.press(KEY_D);
-    Keyboard.release(KEY_D);
+    // Get relative encoder motion since last read
+    encoderValues[i] = encoders[i]->readAndReset();
   }
 
-  if (encVal2 > 0)
+  for (int i = 0; i < ENCODER_COUNT; i++)
   {
-    Keyboard.press(KEY_LEFT_ARROW);
-    Keyboard.release(KEY_LEFT_ARROW);
-  }
-  else if (encVal2 < 0)
-  {
-    Keyboard.press(KEY_RIGHT_ARROW);
-    Keyboard.release(KEY_RIGHT_ARROW);
+    int mapIndex = i * 2;
+    if (encoderValues[i] < 0)
+    {
+      Keyboard.press(ENCODER_MAP[mapIndex]);
+      Keyboard.release(ENCODER_MAP[mapIndex]);
+    }
+    else if (encoderValues[i] > 0)
+    {
+      Keyboard.press(ENCODER_MAP[mapIndex + 1]);
+      Keyboard.release(ENCODER_MAP[mapIndex + 1]);
+    }
   }
 
+  // Move mouse
+  // Mouse.move(encoderValues[0], encoderValues[1]);
+
+  // Read button values
   for (int i = 0; i < BUTTON_COUNT; i++)
   {
     buttons[i]->update();
